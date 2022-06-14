0eef0f338fefdc6c26422ddb8f722e84c8e5cf4a
Junior García <juniorgarciadiaz@outlook.com>
2022-06-14 22:04:32 +0200

    Inicio del proyecto y creación de directorios y ficheros

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Topbar from './components/topbar/topbar';
+import Intro from './components/intro/intro';
+import Product from './components/product/product';
+import Contact from './components/contact/contact';
+//import ('./App.scss');
+
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Product/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/topbar.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Topbar(){
+    return(
+        <div className="topbar">
+            <h1>Hello world</h1>
+        </div>
+    )
+}
